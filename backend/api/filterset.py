3629bedcfe09f873ba1178cee5f76ba1021e92e1
upamid <dstupny@gmail.com>
2021-07-26 21:28:19 +0700

    updated filterset

--- backend/api/filterset.py
+++ backend/api/filterset.py
@@ -1,21 +1,39 @@
 from django.db.models import Q
 from django_filters import rest_framework
+import django_filters
 
-from recipes.models import Recipe
+from recipes.models import Recipe, Tag, Favorite, ShoppingCart
+
 
 class RecipeFilter(rest_framework.FilterSet):
 
-    tags = rest_framework.CharFilter(field_name="tags__slug", method='filter_tags')
+    tags = django_filters.ModelMultipleChoiceFilter(
+        field_name="tags__slug",
+        # method='filter_tags',
+        to_field_name='slug',
+        queryset=Tag.objects.all()
+        )
 
+    is_favorited = django_filters.BooleanFilter(
+        field_name="is_favorited",
+        method='filter_is_favorited',
+    )
+
+    is_in_shopping_cart = django_filters.BooleanFilter(
+        field_name="is_in_shopping_cart",
+        method='filter_is_in_shopping_cart',
+    )
 
     class Meta:
         model = Recipe
-        fields = ['tags']
+        fields = ['tags', 'is_favorited']
 
-    def filter_tags(self, queryset, name, tags):
-        queries = [Q(tags__slug=tag) for tag in tags]
-        query = queries.pop()
-        for item in queries:
-            query |= item
-        return queryset.filter(query)
-        # return queryset.filter(tags__slug__contains=tags.split(','))
+    def filter_is_favorited(self, queryset, name, tags):
+        user = self.request.user
+        fav_recipes = Favorite.objects.filter(fav_user=user).values('fav_item')
+        return queryset.filter(id__in=fav_recipes)
+
+    def filter_is_in_shopping_cart(self, queryset, name, tags):
+        user = self.request.user
+        fav_recipes = ShoppingCart.objects.filter(owner=user).values('item')
+        return queryset.filter(id__in=fav_recipes)
