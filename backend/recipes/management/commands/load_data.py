d3eac5568b98ccbd3ba91ef7918582a5fb40316b
upamid <dstupny@gmail.com>
2021-09-28 01:09:08 +0700

    load fixtures

--- backend/recipes/management/commands/load_data.py
+++ backend/recipes/management/commands/load_data.py
@@ -1,16 +1,24 @@
 import json
+import os
 
-from django.conf import settings
 from django.core.management import BaseCommand
-from django.core import management
-from django.core.management.commands import loaddata
 
-from recipes.models import Recipe
+from recipes.models import Ingredient
+
+fixture_dir = ''
+fixture_filename = 'ingredients.json'
 
 
 class Command(BaseCommand):
     help = "load data"
-    fixture_filename = 'ingerdients.json'
-        
-    def handle(self, path, **options):
-        management.call_command('loaddata', settings.STATIC_ROOT+'/ingerdients.json')
+
+    def handle(self, *args, **options):
+        fixture_file = os.path.join(fixture_dir, fixture_filename)
+        print(fixture_file)
+        f = open(fixture_file,)
+        my_json_obj = json.load(f)
+        for title in my_json_obj:
+            Ingredient.objects.create(
+                name=title['name'],
+                measurement_unit=title['measurement_unit']
+                )
